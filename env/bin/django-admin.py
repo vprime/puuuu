7405d0d64ce04438948c9c5228473abbd69726d9
Vincent Prime <vprime@local-motors.com>
2014-03-30 18:41:33 -0700

    Inital commit of PaperTrail

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Vincent/lm_svn/checkouts/personal/papertrail-django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
