bbf65a6b5dc5a2b5d94f89e5661a4a9b8d25d48d
Vincent Prime <vprime@local-motors.com>
2014-03-30 21:24:40 -0700

    Adds the models, also placeholder page.

--- puuuu/puuuu/urls.py
+++ puuuu/puuuu/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'puuuu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'game.views.index'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
